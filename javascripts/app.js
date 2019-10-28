990ea3b067513a7630a8f0810b4a659997d962b9
Shahriyar <shahryar102@yahoo.com>
2019-10-28 15:09:47 -0400

    this

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,60 +2,39 @@ class Rover {
   constructor(){
     direction:'N'
   }
-<<<<<<< HEAD
-}
-// ======================
-
-// moving part
-function moveForward(rover){
-  console.log("moveForward was called")
-}
-function moveForward(rover){ console.log("moveForward was called") switch (rover.direction){ case "N": rover.y-=1;rover.travelLog.push({x:rover.x , y:rover.y});break; case "E": rover.x+=1;rover.travelLog.push({x:rover.x , y:rover.y});break;
-case "S": rover.y+=1;rover.travelLog.push({x:rover.x , y:rover.y});break;
-case "W": rover.x-=1;rover.travelLog.push({x:rover.x , y:rover.y});break; default: console.log('Error');break; } }
-
-function moveBackward(rover){ console.log("moveBackward was called") switch (rover.direction){ case "N": rover.y+=1;rover.travelLog.push({x:rover.x , y:rover.y});break; case "E": rover.x-=1;rover.travelLog.push({x:rover.x , y:rover.y});break;
-case "S": rover.y-=1;rover.travelLog.push({x:rover.x , y:rover.y});break;
-case "W": rover.x+=1;rover.travelLog.push({x:rover.x , y:rover.y});break; default: console.log('Error');break; } }
-
-// end moving part
-
-  turnLeft() {
-=======
 
   turnLeft(newDirection) {
     this.direction = newDirection;
->>>>>>> 17ba027eb636a9c350089f5ad905ba95455fed29
   }
   
   turnRight(newDirection) {
     this.direction = newDirection;
   }
 
-  moveForward(rover){ 
-    switch (rover.direction){ 
-      case "N": rover.y-=1;rover.travelLog.push({x:rover.x , y:rover.y});
+  moveForward(){ 
+    switch (this.direction){ 
+      case "N": this.y-=1;this.travelLog.push({x:this.x , y:this.y});
       break; 
-      case "E": rover.x+=1;rover.travelLog.push({x:rover.x , y:rover.y});
+      case "E": this.x+=1;this.travelLog.push({x:this.x , y:this.y});
       break;
-      case "S": rover.y+=1;rover.travelLog.push({x:rover.x , y:rover.y});
+      case "S": this.y+=1;this.travelLog.push({x:this.x , y:this.y});
       break;
-      case "W": rover.x-=1;rover.travelLog.push({x:rover.x , y:rover.y});
+      case "W": this.x-=1;this.travelLog.push({x:this.x , y:this.y});
       break; 
       default: console.log('Error');
       break; 
     } 
   }
 
-  moveBackward(rover){
-    switch (rover.direction){ 
-      case "N": rover.y+=1;rover.travelLog.push({x:rover.x , y:rover.y});
+  moveBackward(){
+    switch (this.direction){ 
+      case "N": this.y+=1;this.travelLog.push({x:this.x , y:this.y});
       break; 
-      case "E": rover.x-=1;rover.travelLog.push({x:rover.x , y:rover.y});
+      case "E": this.x-=1;this.travelLog.push({x:this.x , y:this.y});
       break;
-      case "S": rover.y-=1;rover.travelLog.push({x:rover.x , y:rover.y});
+      case "S": this.y-=1;this.travelLog.push({x:this.x , y:this.y});
       break;
-      case "W": rover.x+=1;rover.travelLog.push({x:rover.x , y:rover.y}); 
+      case "W": this.x+=1;this.travelLog.push({x:this.x , y:this.y}); 
       break; 
       default: console.log('Error');
       break; 
