0166819ce2e065e51964f9f1bfd1af2c589de31e
Shahriyar <shahryar102@yahoo.com>
2019-10-28 14:46:41 -0400

    moving part

--- javascripts/app.js
+++ javascripts/app.js
@@ -9,7 +9,16 @@ function turnLeft(rover){
 function turnRight(rover){
   console.log("turnRight was called!");
 }
-
+// moving part
 function moveForward(rover){
   console.log("moveForward was called")
 }
+function moveForward(rover){ console.log("moveForward was called") switch (rover.direction){ case "N": rover.y-=1;rover.travelLog.push({x:rover.x , y:rover.y});break; case "E": rover.x+=1;rover.travelLog.push({x:rover.x , y:rover.y});break;
+case "S": rover.y+=1;rover.travelLog.push({x:rover.x , y:rover.y});break;
+case "W": rover.x-=1;rover.travelLog.push({x:rover.x , y:rover.y});break; default: console.log('Error');break; } }
+
+function moveBackward(rover){ console.log("moveBackward was called") switch (rover.direction){ case "N": rover.y+=1;rover.travelLog.push({x:rover.x , y:rover.y});break; case "E": rover.x-=1;rover.travelLog.push({x:rover.x , y:rover.y});break;
+case "S": rover.y-=1;rover.travelLog.push({x:rover.x , y:rover.y});break;
+case "W": rover.x+=1;rover.travelLog.push({x:rover.x , y:rover.y});break; default: console.log('Error');break; } }
+
+// end moving part
