490405d79b7b2a694380f360abb25a1e2d4e9514
Juan Grajales <juanfe.gl11@gmail.com>
2019-10-28 14:42:06 -0400

    removed started files

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,1 @@
-// Rover Object Goes Here
-// ======================
 
-// ======================
-function turnLeft(rover){
-  console.log("turnLeft was called!");
-}
-
-function turnRight(rover){
-  console.log("turnRight was called!");
-}
-
-function moveForward(rover){
-  console.log("moveForward was called")
-}
