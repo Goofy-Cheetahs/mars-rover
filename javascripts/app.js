b91636c83568283c9b114d6374bb1d3b4c259c62
Shahriyar <shahryar102@yahoo.com>
2019-10-28 15:21:55 -0400

    m

--- javascripts/app.js
+++ javascripts/app.js
@@ -41,13 +41,21 @@ class Rover {
 
   moveForward(){ 
     switch (this.direction){ 
-      case "N": this.y-=1;this.travelLog.push({x:this.x , y:this.y});
+      case "N": 
+      this.y-=1;
+      this.travelLog.push({x:this.x , y:this.y});
       break; 
-      case "E": this.x+=1;this.travelLog.push({x:this.x , y:this.y});
+      case "E": 
+      this.x+=1;
+      this.travelLog.push({x:this.x , y:this.y});
       break;
-      case "S": this.y+=1;this.travelLog.push({x:this.x , y:this.y});
+      case "S": 
+      this.y+=1;
+      this.travelLog.push({x:this.x , y:this.y});
       break;
-      case "W": this.x-=1;this.travelLog.push({x:this.x , y:this.y});
+      case "W": 
+      this.x-=1;
+      this.travelLog.push({x:this.x , y:this.y});
       break; 
       default: console.log('Error');
       break; 
@@ -56,13 +64,20 @@ class Rover {
 
   moveBackward(){
     switch (this.direction){ 
-      case "N": this.y+=1;this.travelLog.push({x:this.x , y:this.y});
+      case "N": this.y+=1;
+      this.travelLog.push({x:this.x , y:this.y});
       break; 
-      case "E": this.x-=1;this.travelLog.push({x:this.x , y:this.y});
+      case "E":
+      this.x-=1;
+      this.travelLog.push({x:this.x , y:this.y});
       break;
-      case "S": this.y-=1;this.travelLog.push({x:this.x , y:this.y});
+      case "S": 
+      this.y-=1;
+      this.travelLog.push({x:this.x , y:this.y});
       break;
-      case "W": this.x+=1;this.travelLog.push({x:this.x , y:this.y}); 
+      case "W":
+      this.x+=1;
+      this.travelLog.push({x:this.x , y:this.y}); 
       break; 
       default: console.log('Error');
       break; 
