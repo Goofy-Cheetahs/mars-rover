985728871ec793ef7cbcd380c9e19831a4c6914e
Juan Grajales <juanfe.gl11@gmail.com>
2019-10-28 15:16:45 -0400

    fix merge issues

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,34 +1,10 @@
 class Rover {
   constructor(direction){
-<<<<<<< HEAD
-    this.direction = direction;
-=======
     this.direction = 'N'
     this.x = 0
     this.y = 0
->>>>>>> cc37523bcc18af5bccf06ce05e3dde1ff103a016
   }
 }
-// ======================
-
-// moving part
-function moveForward(rover){
-  console.log("moveForward was called")
-}
-function moveForward(rover){ console.log("moveForward was called") switch (rover.direction){ case "N": rover.y-=1;rover.travelLog.push({x:rover.x , y:rover.y});break; case "E": rover.x+=1;rover.travelLog.push({x:rover.x , y:rover.y});break;
-case "S": rover.y+=1;rover.travelLog.push({x:rover.x , y:rover.y});break;
-case "W": rover.x-=1;rover.travelLog.push({x:rover.x , y:rover.y});break; default: console.log('Error');break; } }
-
-function moveBackward(rover){ console.log("moveBackward was called") switch (rover.direction){ case "N": rover.y+=1;rover.travelLog.push({x:rover.x , y:rover.y});break; case "E": rover.x-=1;rover.travelLog.push({x:rover.x , y:rover.y});break;
-case "S": rover.y-=1;rover.travelLog.push({x:rover.x , y:rover.y});break;
-case "W": rover.x+=1;rover.travelLog.push({x:rover.x , y:rover.y});break; default: console.log('Error');break; } }
-
-// end moving part
-
-  turnLeft() {
-
-  turnLeft(newDirection) {
-    this.direction = newDirection;
 
   turnLeft() {
   switch(this.direction){
@@ -67,14 +43,8 @@ case "W": rover.x+=1;rover.travelLog.push({x:rover.x , y:rover.y});break; defaul
   }
 
   moveForward(){ 
-<<<<<<< HEAD
-    switch (rover.direction){ 
-      case "N": 
-      rover.y-=1;rover.travelLog.push({x:rover.x , y:rover.y});
-=======
     switch (this.direction){ 
       case "N": this.y-=1;this.travelLog.push({x:this.x , y:this.y});
->>>>>>> cc37523bcc18af5bccf06ce05e3dde1ff103a016
       break; 
       case "E": this.x+=1;this.travelLog.push({x:this.x , y:this.y});
       break;
