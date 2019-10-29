81deebb74ce483b1e69588569f9b950db308ee92
Juan Grajales <juanfe.gl11@gmail.com>
2019-10-29 14:57:34 -0400

    clean code

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,35 +1,25 @@
-// grid
-const grid = [
-    [{x:0,y:0},{x:0,y:1},{x:0,y:2},{x:0,y:3},{x:0,y:4},{x:0,y:5},{x:0,y:6},{x:0,y:7},{x:0,y:8},{x:0,y:9}],
-    [{x:1,y:0},{x:1,y:1},{x:1,y:2},{x:1,y:3},{x:1,y:4},{x:1,y:5},{x:1,y:6},{x:1,y:7},{x:1,y:8},{x:1,y:9}],
-    [{x:2,y:0},{x:2,y:1},{x:2,y:2},{x:2,y:3},{x:2,y:4},{x:2,y:5},{x:2,y:6},{x:2,y:7},{x:2,y:8},{x:2,y:9}],
-    [{x:3,y:0},{x:3,y:1},{x:3,y:2},{x:3,y:3},{x:3,y:4},{x:3,y:5},{x:3,y:6},{x:3,y:7},{x:3,y:8},{x:3,y:9}],
-    [{x:4,y:0},{x:4,y:1},{x:4,y:2},{x:4,y:3},{x:4,y:4},{x:4,y:5},{x:4,y:6},{x:4,y:7},{x:4,y:8},{x:4,y:9}],
-    [{x:5,y:0},{x:5,y:1},{x:5,y:2},{x:5,y:3},{x:5,y:4},{x:5,y:5},{x:5,y:6},{x:5,y:7},{x:5,y:8},{x:5,y:9}],
-    [{x:6,y:0},{x:6,y:1},{x:6,y:2},{x:6,y:3},{x:6,y:4},{x:6,y:5},{x:6,y:6},{x:6,y:7},{x:6,y:8},{x:6,y:9}],
-    [{x:7,y:0},{x:7,y:1},{x:7,y:2},{x:7,y:3},{x:7,y:4},{x:7,y:5},{x:7,y:6},{x:7,y:7},{x:7,y:8},{x:7,y:9}],
-    [{x:8,y:0},{x:8,y:1},{x:8,y:2},{x:8,y:3},{x:8,y:4},{x:8,y:5},{x:8,y:6},{x:8,y:7},{x:8,y:8},{x:8,y:9}],
-    [{x:9,y:0},{x:9,y:1},{x:9,y:2},{x:9,y:3},{x:9,y:4},{x:9,y:5},{x:9,y:6},{x:9,y:7},{x:9,y:8},{x:9,y:9}]
-  ]
-  class Obstacle {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-    }
+/*
+Obstacle class
+- Create an obtacle at position (x,y) 
+*/
+class Obstacle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
-  
+}
 
-  obstacleArr = createObstacles(10)
-  function createObstacles(numObstacles) {
-      let obstacleArr = [];
-      for(let i=0; i<numObstacles; i++){
-      let x = Math.floor(Math.random()*11);
-      let y = Math.floor(Math.random()*11);
-      let obs = new Obstacle(x, y);
-      obstacleArr.push(obs);
-    }
-    return obstacleArr;
+function createObstacles(numObstacles) {
+    let obstacleArr = [];
+    for(let i=0; i<numObstacles; i++){
+    let x = Math.floor(Math.random()*11);
+    let y = Math.floor(Math.random()*11);
+    let obs = new Obstacle(x, y);
+    obstacleArr.push(obs);
   }
+  return obstacleArr;
+}
+
 // rover
 class Rover {
   constructor(direction){
@@ -50,137 +40,149 @@ class Rover {
         return;
       } 
     }
-  }
-  
- 
+}
 
-  turnLeft() {
+turnLeft() {
+switch(this.direction){
+  case "N":
+    this.direction = "W";
+    break;
+  case "W":
+    this.direction = "S";
+    break;
+  case "S":
+    this.direction = "E";
+    break;
+  case "E":
+    this.direction = "N";
+    break;
+  }
+  console.log(`Rover now faces ${this.direction}.`);
+}
+  
+turnRight() {
   switch(this.direction){
     case "N":
-      this.direction = "W";
-      break;
-    case "W":
-      this.direction = "S";
-      break;
-    case "S":
       this.direction = "E";
       break;
     case "E":
+      this.direction = "S";
+      break;
+    case "S":
+      this.direction = "W";
+      break;
+    case "W":
       this.direction = "N";
       break;
-    }
+  }
   console.log(`Rover now faces ${this.direction}.`);
-  }
-  
-  turnRight() {
-    switch(this.direction){
-      case "N":
-        this.direction = "E";
+}
+
+moveForward(){ 
+  switch (this.direction){ 
+    case "N": 
+    this.y-=1;
+    this.checkBoundaries();
+    this.checkObstacles();
+    this.travelLog.push({x:this.x , y:this.y});
+    break; 
+    case "E": 
+    this.x+=1;
+    this.checkBoundaries();
+    this.checkObstacles();
+    this.travelLog.push({x:this.x , y:this.y});
+    break;
+    case "S": 
+    this.y+=1;
+    this.checkBoundaries();
+    this.checkObstacles();
+    this.travelLog.push({x:this.x , y:this.y});
+    break;
+    case "W": 
+    this.x-=1;
+    this.checkBoundaries();
+    this.checkObstacles();
+    this.travelLog.push({x:this.x , y:this.y});
+    break; 
+    default: console.log('Error');
+    break; 
+  } 
+
+  console.log(`Rover position (${this.x}, ${this.y}).`);
+}
+
+moveBackward(){
+  switch (this.direction){ 
+    case "N": 
+    this.y+=1;
+    this.checkBoundaries();
+    this.checkObstacles();
+    this.travelLog.push({x:this.x , y:this.y});
+    break; 
+    case "E":
+    this.x-=1;
+    this.checkBoundaries();
+    this.checkBoundaries();
+    this.travelLog.push({x:this.x , y:this.y});
+    break;
+    case "S": 
+    this.y-=1;
+    this.checkBoundaries();
+    this.checkObstacles();
+    this.travelLog.push({x:this.x , y:this.y});
+    break;
+    case "W":
+    this.x+=1;
+    this.checkBoundaries();
+    this.checkObstacles();
+    this.travelLog.push({x:this.x , y:this.y}); 
+    break; 
+    default: console.log('Error');
+    break; 
+  } 
+  console.log(`Rover position (${this.x}, ${this.y}).`);
+}
+
+// command
+commands(command){
+  console.log('commands was called!');
+
+  for (let i = 0; i < command.length; i++) {
+    switch (command[i]) {
+      case 'f': 
+        this.moveForward();
         break;
-      case "E":
-        this.direction = "S";
+      case 'b': 
+        this.moveBackward();
         break;
-      case "S":
-        this.direction = "W";
+      case 'l': 
+        this.turnLeft();
         break;
-      case "W":
-        this.direction = "N";
+      case 'r': 
+        this.turnRight();
         break;
-    }
-    console.log(`Rover now faces ${this.direction}.`);
-  }
-
-  moveForward(){ 
-    switch (this.direction){ 
-      case "N": 
-      this.y-=1;
-      this.checkBoundaries();
-      this.checkObstacles();
-      this.travelLog.push({x:this.x , y:this.y});
-      break; 
-      case "E": 
-      this.x+=1;
-      this.checkBoundaries();
-      this.checkObstacles();
-      this.travelLog.push({x:this.x , y:this.y});
-      break;
-      case "S": 
-      this.y+=1;
-      this.checkBoundaries();
-      this.checkObstacles();
-      this.travelLog.push({x:this.x , y:this.y});
-      break;
-      case "W": 
-      this.x-=1;
-      this.checkBoundaries();
-      this.checkObstacles();
-      this.travelLog.push({x:this.x , y:this.y});
-      break; 
-      default: console.log('Error');
-      break; 
-    } 
-
-    console.log(`Rover position (${this.x}, ${this.y}).`);
-  }
-
-  moveBackward(){
-    switch (this.direction){ 
-      case "N": 
-      this.y+=1;
-      this.checkBoundaries();
-      this.checkObstacles();
-      this.travelLog.push({x:this.x , y:this.y});
-      break; 
-      case "E":
-      this.x-=1;
-      this.checkBoundaries();
-      this.checkBoundaries();
-      this.travelLog.push({x:this.x , y:this.y});
-      break;
-      case "S": 
-      this.y-=1;
-      this.checkBoundaries();
-      this.checkObstacles();
-      this.travelLog.push({x:this.x , y:this.y});
-      break;
-      case "W":
-      this.x+=1;
-      this.checkBoundaries();
-      this.checkObstacles();
-      this.travelLog.push({x:this.x , y:this.y}); 
-      break; 
-      default: console.log('Error');
-      break; 
-    } 
-    console.log(`Rover position (${this.x}, ${this.y}).`);
-  }
-
-  // command
-  commands(command){
-    console.log('commands was called!');
-
-    for (let i = 0; i < command.length; i++) {
-      switch (command[i]) {
-        case 'f': 
-          this.moveForward();
-          break;
-        case 'b': 
-          this.moveBackward();
-          break;
-        case 'l': 
-          this.turnLeft();
-          break;
-        case 'r': 
-          this.turnRight();
-          break;
-        default:
-          console.log('invalid command');
-          // continue;
-          break;
-      }
+      default:
+        console.log('invalid command');
+        // continue;
+        break;
     }
   }
 }
+}
+
+// // grid
+// const grid = [
+//     [{x:0,y:0},{x:0,y:1},{x:0,y:2},{x:0,y:3},{x:0,y:4},{x:0,y:5},{x:0,y:6},{x:0,y:7},{x:0,y:8},{x:0,y:9}],
+//     [{x:1,y:0},{x:1,y:1},{x:1,y:2},{x:1,y:3},{x:1,y:4},{x:1,y:5},{x:1,y:6},{x:1,y:7},{x:1,y:8},{x:1,y:9}],
+//     [{x:2,y:0},{x:2,y:1},{x:2,y:2},{x:2,y:3},{x:2,y:4},{x:2,y:5},{x:2,y:6},{x:2,y:7},{x:2,y:8},{x:2,y:9}],
+//     [{x:3,y:0},{x:3,y:1},{x:3,y:2},{x:3,y:3},{x:3,y:4},{x:3,y:5},{x:3,y:6},{x:3,y:7},{x:3,y:8},{x:3,y:9}],
+//     [{x:4,y:0},{x:4,y:1},{x:4,y:2},{x:4,y:3},{x:4,y:4},{x:4,y:5},{x:4,y:6},{x:4,y:7},{x:4,y:8},{x:4,y:9}],
+//     [{x:5,y:0},{x:5,y:1},{x:5,y:2},{x:5,y:3},{x:5,y:4},{x:5,y:5},{x:5,y:6},{x:5,y:7},{x:5,y:8},{x:5,y:9}],
+//     [{x:6,y:0},{x:6,y:1},{x:6,y:2},{x:6,y:3},{x:6,y:4},{x:6,y:5},{x:6,y:6},{x:6,y:7},{x:6,y:8},{x:6,y:9}],
+//     [{x:7,y:0},{x:7,y:1},{x:7,y:2},{x:7,y:3},{x:7,y:4},{x:7,y:5},{x:7,y:6},{x:7,y:7},{x:7,y:8},{x:7,y:9}],
+//     [{x:8,y:0},{x:8,y:1},{x:8,y:2},{x:8,y:3},{x:8,y:4},{x:8,y:5},{x:8,y:6},{x:8,y:7},{x:8,y:8},{x:8,y:9}],
+//     [{x:9,y:0},{x:9,y:1},{x:9,y:2},{x:9,y:3},{x:9,y:4},{x:9,y:5},{x:9,y:6},{x:9,y:7},{x:9,y:8},{x:9,y:9}]
+//   ]
 
 
 
@@ -207,5 +209,7 @@ r1.turnLeft();
 r1.moveBackward();
 r1.moveForward();
 r1.commands("rffrfflb");
+
+obstacleArr = createObstacles(10)
 
 /******* testing code *******/
